b9afdf4b77865b5336b90a7e23861de7a063ef9c
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-24 12:24:27 +0600

    Fokira-Marka-Header-complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/Home">Home</a>
+                <a href="/Order">Order</a>
+                <a href="/Inventory">Inventory</a>
+                <a href="/About">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
